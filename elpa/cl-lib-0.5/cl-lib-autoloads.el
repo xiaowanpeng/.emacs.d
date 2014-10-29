ae957f950e00547f1ebfe492a198d9f91bb6d2a1
xiaowp <xiaowanpeng@gmail.com>
2014-10-29 23:22:18 +0800

    add elpa

---
+++ elpa/cl-lib-0.5/cl-lib-autoloads.el
@@ -0,0 +1,25 @@
+;;; cl-lib-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "cl-lib" "cl-lib.el" (21288 20191 299040 663000))
+;;; Generated autoloads from cl-lib.el
+ (let ((d (file-name-directory #$)))
+  (when (member d load-path)
+    (setq load-path (append (remove d load-path) (list d)))))
+
+;;;***
+
+;;;### (autoloads nil nil ("cl-lib-pkg.el") (21288 20191 468665 320000))
+
+;;;***
+
+(provide 'cl-lib-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; cl-lib-autoloads.el ends here
---
+++ elpa/smex-20140131.1006/smex-autoloads.el
@@ -0,0 +1,33 @@
+;;; smex-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (smex-initialize smex) "smex" "smex.el" (21277
+;;;;;;  61854 835355 634000))
+;;; Generated autoloads from smex.el
+
+(autoload 'smex "smex" "\
+
+
+\(fn)" t nil)
+
+(autoload 'smex-initialize "smex" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("smex-pkg.el") (21277 61854 976349 823000))
+
+;;;***
+
+(provide 'smex-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smex-autoloads.el ends here
